db9e693a716770bee36fd4d4f3c204a810871049
ansariashif14 <114073325+ansariashif14@users.noreply.github.com>
2022-12-03 22:59:05 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@ import React, {useEffect, useState} from 'react'
 import '../styles/App.css';
 
 const App = () => {
-  
+//code here 
+
   const [changeStyle,setChangeStyle] = useState("");
   useEffect(() =>{
     setChangeStyle("redColor");
   },[])
   // setChangeStyle("redColor")
-  const toggleHandler = () => {
+  const clickHandle = () => {
   
     if(changeStyle === "redColor"){
       setChangeStyle("blueColor")
@@ -16,12 +17,14 @@ const App = () => {
     else{
       setChangeStyle("redColor")
     }
-  
-//code here 
+      
+    
+    
+  }
   return (
     <div id="main">
-      <p className={changeStyle}>Newton School</p>
-      <button id='button' onClick={toggleHandler}>Change Style</button>
+      <p className={changeStyle} >Newton School</p>
+      <button id='button' onClick={clickHandle}>Change Style</button>
     </div>
   )
 }
